731ee135e03d94b84fae6bc41c0d222091409358
Gops <adeGopal@gmail.com>
2024-09-19 08:37:42 +0530

    commited

---
+++ src/logs4j_demo/Demo.java
@@ -0,0 +1,14 @@
+package logs4j_demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver; 
+
+public class Demo {
+
+	public static void main(String[] args) {
+System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+	}
+
+}
+ 
