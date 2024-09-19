5b3e5cbf2282de000d093a1b51d27c129cd62ecd
karan <karan@gmail.com>
2024-09-19 09:26:28 +0530

    second time updated

--- src/logs4j_demo/Demo.java
+++ src/logs4j_demo/Demo.java
@@ -9,6 +9,8 @@ public class Demo {
 System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
 		WebDriver driver =new ChromeDriver();
 		System.out.println("karan has updated code");
+		System.out.println("karan has 2 time updated code");
+
 	       System.out.println("krish has updated code and commited");
 
 		
